7e97557f4220845605d0e92780d71f271dea7ead
rinrab <tima@chemodax.net>
2022-11-30 21:09:39 +0300

    Change mark position

--- js/gamestate.js
+++ js/gamestate.js
@@ -240,8 +240,8 @@ class Game {
 		this.width = this.windscenario.width;
 		this.height = this.windscenario.height;
 		this.marks = [
-			{ "x": 5, "y": this.height - 2, "type": Marks.startleft },
-			{ "x": this.width - 5, "y": this.height - 2, "type": Marks.startright },
+			{ "x": 7, "y": this.height - 2, "type": Marks.startleft },
+			{ "x": this.width - 7, "y": this.height - 2, "type": Marks.startright },
 			{ "x": this.width / 2, "y": 2, "type": Marks.mark1 },
 		];
 	}
