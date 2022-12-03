314f18961d909fd38798de287b533e5faf7f0d06
rinrab <tima@chemodax.net>
2022-12-03 18:36:09 +0300

    Add show ezual lines

--- js/settings.js
+++ js/settings.js
@@ -2,6 +2,7 @@ var localStorageNames = {
     settingsshowboats: "settings-show-boats",
     settingsshowtracks: "settings-show-tarcks",
     settingsshowlanelines: "settings-show-lanelines",
+    settingsshowequallines: "equal-lines",
     wind: "wind-data#",
     windlist: "wind-list",
     selectedWind: "selected-wind",
@@ -10,16 +11,19 @@ var defaultSetting = {
     showboats: true,
     showtracks: true,
     showlaneline: true,
+    showequallines: false,
 };
 class Settings {
     showboats;
     showtracks;
     showlaneline;
+    showequallines;
 }
 
 var setShowBoatsCheck;
 var setShowTracksCheck;
 var setShowLanelinesCheck;
+var setShowEqualLines;
 
 var settings = defaultSetting;
 
@@ -27,24 +31,28 @@ function settingsInit() {
     loadSettings();
 
     setShowBoatsCheck = document.getElementById("set-show-boats");
-    setShowTracksCheck = document.getElementById("set-show-tracks")
-    setShowLanelinesCheck = document.getElementById("set-show-lanelines")
+    setShowTracksCheck = document.getElementById("set-show-tracks");
+    setShowLanelinesCheck = document.getElementById("set-show-lanelines");
+    setShowEqualLines = document.getElementById("set-show-equal-lines");
 
     setShowBoatsCheck.addEventListener("click", settingsChanged);
     setShowTracksCheck.addEventListener("click", settingsChanged);
     setShowLanelinesCheck.addEventListener("click", settingsChanged);
+    setShowEqualLines.addEventListener("click", settingsChanged);
 }
 
 function saveSettings() {
     localStorage.setItem(localStorageNames.settingsshowboats, settings.showboats)
     localStorage.setItem(localStorageNames.settingsshowtracks, settings.showtracks)
     localStorage.setItem(localStorageNames.settingsshowlanelines, settings.showlaneline);
+    localStorage.setItem(localStorageNames.settingsshowequallines, settings.showequallines);
 }
 
 function loadSettings() {
     settings.showboats = readBoolSettings(localStorageNames.settingsshowboats, defaultSetting.showboats);
     settings.showtracks = readBoolSettings(localStorageNames.settingsshowtracks, defaultSetting.showtracks);
     settings.showlaneline = readBoolSettings(localStorageNames.settingsshowlanelines, defaultSetting.showlaneline);
+    settings.showequallines = readBoolSettings(localStorageNames.settingsshowequallines, defaultSetting.showequallines);
 }
 
 function readBoolSettings(settingName, defaultValue) {
@@ -62,6 +70,7 @@ function settingsChanged() {
     settings.showboats = setShowBoatsCheck.checked;
     settings.showtracks = setShowTracksCheck.checked;
     settings.showlaneline = setShowLanelinesCheck.checked;
+    settings.showequallines = setShowEqualLines.checked;
 
     saveSettings();
     applySettings();
@@ -89,6 +98,12 @@ function applySettings() {
         for (var i = 0; i < game.players.length; i++) {
             game.players[i].html.children[0].innerHTML = boathidesvg;
         }
+    }
+    setShowEqualLines.checked = settings.showequallines;
+    if (settings.showequallines) {
+        document.getElementById("lines-container").hidden = false;
+    } else {
+        document.getElementById("lines-container").hidden = true;
     }
 }
 
