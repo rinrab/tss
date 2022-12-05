b2c75b1ac83657d396a19bb6bca24dae93cdc000
rinrab <tima@chemodax.net>
2022-12-05 13:13:36 +0300

    Move images from controls.js to glyphs,js

--- js/controlls.js
+++ js/controlls.js
@@ -41,83 +41,11 @@ function addControll(player) {
     controlls.insertBefore(newControlGroup1, document.getElementById("last-controll"));
 
     var labelsStart = ["L", "M", "R"];
-    var labelsRace = [`
-<svg xmlns="http://www.w3.org/2000/svg"
-     class="port-forward-btn"
-     viewBox="0 0 16 16" width="16" height="16" >
-        < g >
-      <polyline points="2,14 14,2"
-                fill="none" 
-                stroke="currentColor"
-                stroke-linejoin="miter"
-                stroke-width="1.7"/>
+    var labelsRace = [
+        Glyphs.portForward + Glyphs.starboardForward,
+        Glyphs.portTack + Glyphs.starboardTack,
+        "&#8857;&#8592;"];
 
-      <polyline points="13,8 14,2 8,3" 
-                fill="none"
-                stroke="currentColor" 
-                stroke-linejoin="miter"
-                stroke-linecap="round"
-                stroke-width="1.7"/>
-
-      <ellipse rx="1.8" ry="1.8" cx="2" cy="14" fill="currentColor" />
-    </g >
-</svg>
-<svg xmlns="http://www.w3.org/2000/svg"
-     class="starboard-forward-btn"
-     viewBox="0 0 16 16" width="16" height="16" >
-        < g >
-      <polyline points="14,14 2,2"
-                fill="none" 
-                stroke="currentColor"
-                stroke-linejoin="miter"
-                stroke-width="1.7"/>
-
-      <polyline points="3,8 2,2 8,3" 
-                fill="none"
-                stroke="currentColor" 
-                stroke-linejoin="miter"
-                stroke-linecap="round"
-                stroke-width="1.7"/>
-
-      <ellipse rx="1.8" ry="1.8" cx="14" cy="14" fill="currentColor" />
-    </g >
-</svg>
-`,
-        `<svg class="port-tack-btn"
-     viewBox="0 0 16 16" width="16" height="16" xmlns="http://www.w3.org/2000/svg">
-    <g>
-      <polyline points="4,14 10,8 4,2" 
-                fill="none" 
-                stroke="currentColor" 
-                stroke-linejoin="miter"
-                stroke-width="1.7"/>
-
-      <polyline points="4,7 3,1 9,2" 
-                fill="none" stroke="currentColor" 
-                stroke-linejoin="miter"
-                stroke-linecap="round"
-                stroke-width="1.7"/>
-      <ellipse rx="1.8" ry="1.8" cx="4" cy="14" fill="currentColor" />
-    </g>
-</svg>
-<svg class="starboard-tack-btn" viewBox="0 0 16 16" width="16" height="16" xmlns="http://www.w3.org/2000/svg">
-    <g>
-      <polyline points="12,14 6,8 12,2" 
-                fill="none" 
-                stroke="currentColor" 
-                stroke-linejoin="miter"
-                stroke-width="1.7"/>
-
-      <polyline points="12,7 13,1 7,2" 
-                fill="none" stroke="currentColor" 
-                stroke-linejoin="miter"
-                stroke-linecap="round"
-                stroke-width="1.7"/>
-      <ellipse rx="1.8" ry="1.8" cx="12" cy="14" fill="currentColor" />
-    </g>
-</svg>
-
-`, "&#8857;&#8592;"];
     var tooltips = ["Start left", "Start middle", "Start right"];
 
     for (var i = 0; i < 2; i++) {
---
+++ js/glyphs.js
@@ -0,0 +1,84 @@
+const Glyphs = {
+    starboardForward: `
+    <svg xmlns="http://www.w3.org/2000/svg"
+         class="starboard-forward-btn"
+         viewBox="0 0 16 16" width="16" height="16" >
+            <g>
+          <polyline points="14,14 2,2"
+                    fill="none" 
+                    stroke="currentColor"
+                    stroke-linejoin="miter"
+                    stroke-width="1.7"/>
+
+          <polyline points="3,8 2,2 8,3" 
+                    fill="none"
+                    stroke="currentColor" 
+                    stroke-linejoin="miter"
+                    stroke-linecap="round"
+                    stroke-width="1.7"/>
+
+          <ellipse rx="1.8" ry="1.8" cx="14" cy="14" fill="currentColor" />
+        </g>
+    </svg>`,
+    portForward: `
+    <svg xmlns="http://www.w3.org/2000/svg"
+         class="port-forward-btn"
+         viewBox="0 0 16 16" width="16" height="16" >
+            <g>
+          <polyline points="2,14 14,2"
+                    fill="none" 
+                    stroke="currentColor"
+                    stroke-linejoin="miter"
+                    stroke-width="1.7"/>
+
+          <polyline points="13,8 14,2 8,3" 
+                    fill="none"
+                    stroke="currentColor" 
+                    stroke-linejoin="miter"
+                    stroke-linecap="round"
+                    stroke-width="1.7"/>
+
+          <ellipse rx="1.8" ry="1.8" cx="2" cy="14" fill="currentColor" />
+        </g>
+    </svg>
+    `,
+    portTack: `<svg class="port-tack-btn"
+                viewBox="0 0 16 16" 
+                width="16" height="16"
+                xmlns="http://www.w3.org/2000/svg">
+     <g>
+       <polyline points="4,14 10,8 4,2" 
+                 fill="none" 
+                 stroke="currentColor" 
+                 stroke-linejoin="miter"
+                 stroke-width="1.7"/>
+
+       <polyline points="4,7 3,1 9,2" 
+                 fill="none" stroke="currentColor" 
+                 stroke-linejoin="miter"
+                 stroke-linecap="round"
+                 stroke-width="1.7"/>
+       <ellipse rx="1.8" ry="1.8" cx="4" cy="14" fill="currentColor" />
+     </g>
+    </svg>`,
+    starboardTack: `
+    <svg class="starboard-tack-btn"
+                viewBox="0 0 16 16"
+                width="16" height="16" 
+                xmlns="http://www.w3.org/2000/svg">
+        <g>
+          <polyline points="12,14 6,8 12,2" 
+                    fill="none" 
+                    stroke="currentColor" 
+                    stroke-linejoin="miter"
+                    stroke-width="1.7"/>
+    
+          <polyline points="12,7 13,1 7,2" 
+                    fill="none" stroke="currentColor" 
+                    stroke-linejoin="miter"
+                    stroke-linecap="round"
+                    stroke-width="1.7"/>
+          <ellipse rx="1.8" ry="1.8" cx="12" cy="14" fill="currentColor" />
+        </g>
+    </svg>`
+};
