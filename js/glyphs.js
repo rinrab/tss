3f5ed13bd7a8d3bbc0ee9ce94a34ff95d047af66
Ivan.Z <53048645+chemodax@users.noreply.github.com>
2022-12-05 15:54:37 +0300

    Use svg symbols for glyphs.

--- js/glyphs.js
+++ js/glyphs.js
@@ -1,84 +1,21 @@
 const Glyphs = {
     starboardForward: `
-    <svg xmlns="http://www.w3.org/2000/svg"
-         class="starboard-forward-btn"
-         viewBox="0 0 16 16" width="16" height="16" >
-            <g>
-          <polyline points="14,14 2,2"
-                    fill="none" 
-                    stroke="currentColor"
-                    stroke-linejoin="miter"
-                    stroke-width="1.7"/>
-
-          <polyline points="3,8 2,2 8,3" 
-                    fill="none"
-                    stroke="currentColor" 
-                    stroke-linejoin="miter"
-                    stroke-linecap="round"
-                    stroke-width="1.7"/>
-
-          <ellipse rx="1.8" ry="1.8" cx="14" cy="14" fill="currentColor" />
-        </g>
+    <svg xmlns="http://www.w3.org/2000/svg" class="starboard-forward-btn icon-sm">
+        <use xlink:href="#icon-starboard-forward" />
     </svg>`,
+
     portForward: `
-    <svg xmlns="http://www.w3.org/2000/svg"
-         class="port-forward-btn"
-         viewBox="0 0 16 16" width="16" height="16" >
-            <g>
-          <polyline points="2,14 14,2"
-                    fill="none" 
-                    stroke="currentColor"
-                    stroke-linejoin="miter"
-                    stroke-width="1.7"/>
-
-          <polyline points="13,8 14,2 8,3" 
-                    fill="none"
-                    stroke="currentColor" 
-                    stroke-linejoin="miter"
-                    stroke-linecap="round"
-                    stroke-width="1.7"/>
-
-          <ellipse rx="1.8" ry="1.8" cx="2" cy="14" fill="currentColor" />
-        </g>
-    </svg>
-    `,
-    portTack: `<svg class="port-tack-btn"
-                viewBox="0 0 16 16" 
-                width="16" height="16"
-                xmlns="http://www.w3.org/2000/svg">
-     <g>
-       <polyline points="8,14 14,8 8,2" 
-                 fill="none" 
-                 stroke="currentColor" 
-                 stroke-linejoin="miter"
-                 stroke-width="1.7"/>
-
-       <polyline points="8,7 7,1 13,2" 
-                 fill="none" stroke="currentColor" 
-                 stroke-linejoin="miter"
-                 stroke-linecap="round"
-                 stroke-width="1.7"/>
-       <ellipse rx="1.8" ry="1.8" cx="8" cy="14" fill="currentColor" />
-     </g>
+    <svg xmlns="http://www.w3.org/2000/svg" class="port-forward-btn icon-sm">
+        <use xlink:href="#icon-port-forward" />
     </svg>`,
-    starboardTack: `
-    <svg class="starboard-tack-btn"
-                viewBox="0 0 16 16"
-                width="16" height="16" 
-                xmlns="http://www.w3.org/2000/svg">
-        <g>
-          <polyline points="8,14 2,8 8,2" 
-                    fill="none" 
-                    stroke="currentColor" 
-                    stroke-linejoin="miter"
-                    stroke-width="1.7"/>
 
-          <polyline points="8,7 9,1 3,2" 
-                    fill="none" stroke="currentColor" 
-                    stroke-linejoin="miter"
-                    stroke-linecap="round"
-                    stroke-width="1.7"/>
-          <ellipse rx="1.8" ry="1.8" cx="8" cy="14" fill="currentColor" />
-        </g>
-    </svg>`
+    portTack: `
+        <svg xmlns="http://www.w3.org/2000/svg" class="port-tack-btn icon-sm">
+            <use xlink:href="#icon-port-tack" />
+        </svg>`,
+
+    starboardTack: `
+        <svg xmlns="http://www.w3.org/2000/svg" class="starboard-tack-btn icon-sm">
+            <use xlink:href="#icon-starboard-tack" />
+        </svg>`,
 };
