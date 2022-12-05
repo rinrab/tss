0aab64314e77bde89be2f3bcf463a55bf3bd6395
Ivan.Z <53048645+chemodax@users.noreply.github.com>
2022-12-05 15:39:11 +0300

    Center tack glyphs.

--- js/glyphs.js
+++ js/glyphs.js
@@ -47,18 +47,18 @@ const Glyphs = {
                 width="16" height="16"
                 xmlns="http://www.w3.org/2000/svg">
      <g>
-       <polyline points="4,14 10,8 4,2" 
+       <polyline points="8,14 14,8 8,2" 
                  fill="none" 
                  stroke="currentColor" 
                  stroke-linejoin="miter"
                  stroke-width="1.7"/>
 
-       <polyline points="4,7 3,1 9,2" 
+       <polyline points="8,7 7,1 13,2" 
                  fill="none" stroke="currentColor" 
                  stroke-linejoin="miter"
                  stroke-linecap="round"
                  stroke-width="1.7"/>
-       <ellipse rx="1.8" ry="1.8" cx="4" cy="14" fill="currentColor" />
+       <ellipse rx="1.8" ry="1.8" cx="8" cy="14" fill="currentColor" />
      </g>
     </svg>`,
     starboardTack: `
@@ -67,18 +67,18 @@ const Glyphs = {
                 width="16" height="16" 
                 xmlns="http://www.w3.org/2000/svg">
         <g>
-          <polyline points="12,14 6,8 12,2" 
+          <polyline points="8,14 2,8 8,2" 
                     fill="none" 
                     stroke="currentColor" 
                     stroke-linejoin="miter"
                     stroke-width="1.7"/>
-    
-          <polyline points="12,7 13,1 7,2" 
+
+          <polyline points="8,7 9,1 3,2" 
                     fill="none" stroke="currentColor" 
                     stroke-linejoin="miter"
                     stroke-linecap="round"
                     stroke-width="1.7"/>
-          <ellipse rx="1.8" ry="1.8" cx="12" cy="14" fill="currentColor" />
+          <ellipse rx="1.8" ry="1.8" cx="8" cy="14" fill="currentColor" />
         </g>
     </svg>`
 };
