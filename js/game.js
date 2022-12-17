98b00663488285c11d6f09bf1e8003698f0f4079
rinrab <tima@chemodax.net>
2022-12-17 19:04:25 +0300

    Remove coments

--- js/game.js
+++ js/game.js
@@ -502,24 +502,22 @@ function init() {
     fullscreenToggle.addEventListener("click", function () {
         var elem = document.documentElement;
 
-        /* View in fullscreen */
         function openFullscreen() {
             if (elem.requestFullscreen) {
                 elem.requestFullscreen();
-            } else if (elem.webkitRequestFullscreen) { /* Safari */
+            } else if (elem.webkitRequestFullscreen) {
                 elem.webkitRequestFullscreen();
-            } else if (elem.msRequestFullscreen) { /* IE11 */
+            } else if (elem.msRequestFullscreen) {
                 elem.msRequestFullscreen();
             }
         }
 
-        /* Close fullscreen */
         function closeFullscreen() {
             if (document.exitFullscreen) {
                 document.exitFullscreen();
-            } else if (document.webkitExitFullscreen) { /* Safari */
+            } else if (document.webkitExitFullscreen) {
                 document.webkitExitFullscreen();
-            } else if (document.msExitFullscreen) { /* IE11 */
+            } else if (document.msExitFullscreen) {
                 document.msExitFullscreen();
             }
         }
